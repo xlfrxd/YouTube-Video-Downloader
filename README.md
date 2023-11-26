70ba26c69cd2141581b0b72606dc82139fe2c9ba
xlfrxd <alfred_victoria@dlsu.edu.ph>
2023-11-26 21:58:08 +0800

    UI changes, updated title and readme to inform max res of 720p30.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # YouTube
  A YouTube video downloader because I don't have YouTube premium anymore
+
+ The library, pyTube, can only download videos at 720p30fps and below. Thus, this app can only download videos at the same resolution.
--- main.py
+++ main.py
@@ -5,34 +5,37 @@ import customtkinter # GUI Library from CustomTkinter by tomschimansky
 import ssl # SSL Certificate Verification
 
 
-def downloadVideo():
-
-    yt = YouTube(link_var.get())
-
-    updateStatus("Downloading video: \"" + yt.title + "\"")
+def downloadVideo(link):
 
     try:
+        yt = YouTube(link)
+
+        updateStatus("Downloading video: \"" + yt.title + "\"")
     
         yt.streams.get_highest_resolution().download("./videos")
 
+        updateStatus("Download complete!")  
     except:
         updateStatus("Download failed!")
-
-    updateStatus("Download complete!")
+    
 
 def updateStatus(updated_txt):
     statusLbl.configure(text=updated_txt)
+
+def downloadClicked():
+    downloadVideo(linkEntry.get())
+    linkEntry.focus()
 
 # Main Loop
 
 # Disable SSL certificate verification
 ssl._create_default_https_context = ssl._create_unverified_context # Bypasses Certificate Verification Clearance
 
-# Root window
+
+# Root 
 root = customtkinter.CTk()
-root.geometry("500x350")
 
-
+# Main Properties
 customtkinter.set_appearance_mode("system")
 customtkinter.set_default_color_theme("dark-blue")
 
@@ -41,33 +44,33 @@ root.title("YouTube Video Downloader by xlfrxd")
 root.geometry('400x250')
 
 
+
 # Main Frame
 mainFrame = customtkinter.CTkFrame(master=root)
-mainFrame.grid()
+mainFrame.pack(padx=0, pady=0, expand=True, fill="both")
+
 
 # Widgets
 
 # Title Label
-titleLbl = customtkinter.CTkLabel(mainFrame, text = "YouTube Video Downloader")
-titleLbl.grid(row=0,column=1)
+titleLbl = customtkinter.CTkLabel(mainFrame, text = "YouTube Video Downloader (.mp4)", font=("Helvetica Neue",20))
+titleLbl.pack(padx=5, expand=True, fill="both")
 
-# Link Label
-linkLbl = customtkinter.CTkLabel(mainFrame, text = "Link: ")
-linkLbl.grid(row=1,column=0)
+# Subtitle Label
+subtitleLbl = customtkinter.CTkLabel(mainFrame, text = "by xlfrxd", font=("Helvetica Neue",12))
+subtitleLbl.pack(padx=5, pady=0, expand=True, fill="both")
 
 # Link Textbox
-link_var = StringVar()
-linkEntry = customtkinter.CTkEntry(mainFrame, textvariable=link_var)
-link_var.set("")
-linkEntry.grid(row=1,column=1)
+linkEntry = customtkinter.CTkEntry(mainFrame, placeholder_text="YouTube Link")
+linkEntry.pack(padx=5, pady=10, expand=True, fill="both")
 
 # Download Button
-downloadBtn = customtkinter.CTkButton(mainFrame, text = "Download", command=downloadVideo)
-downloadBtn.grid(row=2, column=1)
+downloadBtn = customtkinter.CTkButton(mainFrame, text = "Download", command=downloadClicked)
+downloadBtn.pack(padx=5, pady=10, expand=True, fill="both")
 
 # Status Label
 statusLbl = customtkinter.CTkLabel(mainFrame, text = "")
-statusLbl.grid(row=3,column=1)
+statusLbl.pack(padx=5, pady=10, expand=True, fill="both")
 
 
 # Start GUI
